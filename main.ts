258b2ad96eea56d2bd8c83c24479eeb58f006002
raeperd <raeperd117@gmail.com>
2022-05-31 16:59:03 +0900

    First commit with manifest.json

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
+export default class DailyNoteExtendedPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
@@ -108,9 +108,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: DailyNoteExtendedPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: DailyNoteExtendedPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
